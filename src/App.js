1c16733d0fb7ccc0e438aa2c2da1fceef082b943
MarcoContreras2323 <marco.contreras@aragoconsulting.com>
2023-06-14 14:51:39 -0500

    clean code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-/*import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Menu from './components/Menu/Menu'
-import './App.css';
-import "@fortawesome/fontawesome-free/css/all.css";
-
-
-const App = () => {
-  return (
-    <Router>
-      <div className="app">
-        <Menu />
-      </div>
-    </Router>
-  );
-};
-
-export default App;*/
 import React, { useState } from 'react';
 import Login from './components/login/Login';
 import Menu from './components/Menu/Menu';
